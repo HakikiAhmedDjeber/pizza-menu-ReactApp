e9af07ecc66ae6c3d010a1f0b6b0d9c2fc1f57fb
HakikiAhmedDjeber <hakikiahmeddjeber@gmail.com>
2023-10-09 12:22:01 +0100

    first show of element with react

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
 import React from "react";
-import { ReactDOM } from "react-dom/client";
+import ReactDOM from "react-dom/client";
+
+function App() {
+  return <h1>weee REACT</h1>;
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
